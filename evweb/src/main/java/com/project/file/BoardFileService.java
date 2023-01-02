96586eac525acd083021ef61d3b5f116ca8ec3ed
Morning <jangsj93@gmail.com>
2023-01-02 15:36:12 +0900

    test

--- evweb/src/main/java/com/project/file/BoardFileService.java
+++
@@ -1,9 +0,0 @@
-package com.project.file;
-
-import java.util.List;
-
-public interface BoardFileService {
-	//---------------첨부파일----------------
-	List<BoardFileDTO> getFileList(String board_no);
-	BoardFileDTO getFile(BoardFileDTO inputdata);
-}
